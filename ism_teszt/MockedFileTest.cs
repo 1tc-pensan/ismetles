cfd1c3640f2896b38f3ff54acd2562cdb362203b
1t-pensan <ppatrik0319@gmail.com>
2025-11-17 11:14:11 +0100

    ll

--- ism_teszt/MockedFileTest.cs
+++ ism_teszt/MockedFileTest.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using ism_console;
+using ism_core;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace ism_teszt
 {
-    internal class MockedFileTest
+    public class MockedFileTests
     {
+        [Fact]
+        public void ExportUsersToCsv_ShouldWriteCorrectContent()
+        {
+            // Arrange
+            var users = new List<User>
+            {
+                new User(
+                    1,
+                    "Teszt Elek",
+                    "pw123",
+                    "teszt@gmail.com",
+                    DateTime.Now.ToString(),
+                    3
+                )
+            };
+
+            using var ms = new MemoryStream();
+            using var sw = new StreamWriter(ms, Encoding.UTF8);
+
+            // Act – nem fájlba írunk, hanem memóriába
+            foreach (var user in users)
+            {
+                string line =$"{user.Id};{user.Name};{user.Password};{user.Email}; {user.RegiDate:yyyy-MM-dd HH:mm:ss}{user.Level}";
+
+                sw.WriteLine(line);
+            }
+
+            sw.Flush();
+
+            // Assert
+            ms.Position = 0;
+            using var sr = new StreamReader(ms);
+            string content = sr.ReadToEnd();
+
+            Assert.Contains("Teszt Elek", content);
+            Assert.Contains("pw123", content);
+            Assert.Contains("teszt@gmail.com", content);
+        }
     }
 }

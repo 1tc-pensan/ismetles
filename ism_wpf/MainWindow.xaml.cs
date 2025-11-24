44d4da23555c84f88b703b648330f925c9bca29a
1t-pensan <ppatrik0319@gmail.com>
2025-11-24 11:08:27 +0100

    asd

--- ism_wpf/MainWindow.xaml.cs
+++ ism_wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ism_core;
 
 namespace ism_wpf
 {
@@ -16,14 +18,20 @@ namespace ism_wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<User> users;
+        private UserService userService;
         public MainWindow()
         {
             InitializeComponent();
+            users = new ObservableCollection<User>();
+            userService = new UserService(users.ToList());
+            dgUsers.ItemsSource = users;
         }
 
         private void AddUserButton_Click(object sender, RoutedEventArgs e)
         {
-
+            var newUser=userService.CreateUser("Új név","jelszo","email@gmail.com", "2024-01-01", "1");
+            users.Add(newUser);
         }
 
         private void DeleteUserButton_Click(object sender, RoutedEventArgs e)
@@ -38,7 +46,14 @@ namespace ism_wpf
 
         private void LoadButton_Click(object sender, RoutedEventArgs e)
         {
-
+            string filePath = Config.UserFilePath;
+            char separator = Config.CsvSeparator;
+            users.Clear();
+            userService.LoadFromFile(filePath, separator);
+            foreach (var user  in userService.GetAllUsers())
+            {
+                users.Add(user);
+            }
         }
     }
 }
